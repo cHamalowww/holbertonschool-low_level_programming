a570ef9a438fe06a94eb7c2e41d7161c4e722b44
cHamalowww <9408@holbertonstudents.com>
2024-06-18 11:43:18 +0000

    My commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {	
-	printf("%zu\n",sizeof(int));
-	printf("%zu\n",sizeof(char));
-	printf("%zu\n",sizeof(long int));
-	printf("%zu\n",sizeof(long long int));
-	printf("%zu\n",sizeof(float));
+	printf("Size of a int:%lu\n",sizeof(int));
+	printf("Size of a char:%lu\n",sizeof(char));
+	printf("Size of a long int:%lu\n",sizeof(long int));
+	printf("Size of a long long int:%lu\n",sizeof(long long int));
+	printf("Size of a float:%lu\n",sizeof(float));
 	return (0);
 }
