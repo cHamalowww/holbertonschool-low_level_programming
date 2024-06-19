0ed6f0fdda0639139296ccc3476e9399e0535fc2
cHamalowww <9408@holbertonstudents.com>
2024-06-19 09:01:29 +0000

    My commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ( n>0 )
-		printf("%d is positive\n")
+		printf("%lu is positive\n");
 	if ( n<0 )
-		printf("%d is negative\n")
+		printf("%lu is negative\n");
 	if ( n=0 )
-		printf("%d is zero")
+		printf("%lu is zero");
 	return (0);
 }
