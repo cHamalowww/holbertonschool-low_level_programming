7eb21b08931cab2452d3e70578887b403e13153a
cHamalowww <9408@holbertonstudents.com>
2024-06-20 11:29:08 +0000

    My commit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n <= 6; is++)
+
+	if (n == 0)
+		printf("Last digit of %d is\n" n)
+	else if (n <= 6)
+		printf("Last digit of %d is\n" n)
+	else
+		print("Last digit of %d is\n" n)
+
+	return (0);
+}
