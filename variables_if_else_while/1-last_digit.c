f8902958096e4672344e5260e1eff78c3919c1c5
cHamalowww <9408@holbertonstudents.com>
2024-06-21 10:12:43 +0000

    My commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-		printf("%d and is 0 \n", n % 10);
+		printf("%d and is 0\n", n % 10);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0 \n", n % 10);
+		printf("%d and is less than 6 and not 0\n", n % 10);
 	}
 
 	return (0);
