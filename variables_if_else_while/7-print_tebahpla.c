248b98c7b130f613c8a7417e1ff12c84f5217941
cHamalowww <9408@holbertonstudents.com>
2024-06-21 13:07:30 +0000

    My commit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hpla;
+
+	for (hpla = 97; hpla <= 122; hpla--)
+	{
+		putchar(hpla);
+	}
+	putchar('\n');
+	return (0);
+}
